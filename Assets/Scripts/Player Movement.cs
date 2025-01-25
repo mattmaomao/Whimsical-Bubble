0bd52c51c28613529159e8c1fd2ca683ada24b70
mattmaomao <matthewcp220@gmail.com>
2025-01-25 16:42:49 +0800

    add bubble tempalte

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] bool moving = false;
     [SerializeField] float moveSpeed = 5f;
 
     void Update()
     {
-        autoMove();
+        if (moving)
+            autoMove();
     }
 
     void autoMove()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+
+    // trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bubble"))
+        {
+            other.GetComponent<Bubble>().trigger();
+        }
     }
 }
