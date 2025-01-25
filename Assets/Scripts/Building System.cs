692a40b28da3dbd651a3a3707a9586d13ad831ae
mattmaomao <matthewcp220@gmail.com>
2025-01-25 17:22:46 +0800

    check placment

--- Assets/Scripts/Building System.cs
+++ Assets/Scripts/Building System.cs
@@ -16,11 +16,51 @@ public class BuildingSystem : MonoBehaviour
 
     void Start()
     {
+        selectingBubble = -1;
+        cursorBubbleImg.SetActive(false);
     }
 
     void Update()
     {
-        if (selectingBubble != -1)
+        checkPlaceBubble();
+    }
+
+    public void resetBuilding(float currency = 100)
+    {
+        this.currency = currency;
+        currencyText.text = currency.ToString();
+        foreach (Transform child in bubbleContainer)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    void checkPlaceBubble()
+    {
+        if (Input.mousePosition.y > Screen.height - 120)
+            return;
+
+        if (selectingBubble == 99)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // snap posisiton to grid
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.x = Mathf.Round(mousePos.x + .5f) - .5f;
+                mousePos.y = Mathf.Round(mousePos.y);
+                mousePos.z = 0;
+
+                // check if the position is valid
+                RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+                if (hit.collider != null && hit.collider.CompareTag("bubble"))
+                {
+                    hit.collider.GetComponent<Bubble>().destroy();
+                    currency += bubbleManager.bubbleData[selectingBubble].price;
+                    currencyText.text = currency.ToString();
+                }
+            }
+        }
+        else if (selectingBubble != -1)
         {
             // show bubble on cursor
             cursorBubbleImg.SetActive(true);
@@ -33,39 +73,37 @@ public class BuildingSystem : MonoBehaviour
                 {
                     // snap posisiton to grid
                     Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    mousePos.x = Mathf.Round(mousePos.x);
+                    mousePos.x = Mathf.Round(mousePos.x + .5f) - .5f;
                     mousePos.y = Mathf.Round(mousePos.y);
                     mousePos.z = 0;
 
-                    // todo: check if the position is valid
+                    // check if the position is valid
+                    RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+                    if (hit.collider != null && (hit.collider.CompareTag("bubble") || hit.collider.CompareTag("wall")))
+                    {
+                        Debug.Log("Cannot place object here. 'bubble' or 'wall' object present.");
+                    }
+                    else
+                    {
+                        // Instantiate bubble
+                        GameObject temp = Instantiate(bubbleManager.bubbleData[selectingBubble].prefab, mousePos, Quaternion.identity);
+                        temp.transform.SetParent(bubbleContainer);
+                        Bubble bubble = temp.GetComponent<Bubble>();
+                        bubble.init((BubbleType)selectingBubble);
 
-                    // Instantiate bubble
-                    GameObject temp = Instantiate(bubbleManager.bubbleData[selectingBubble].prefab, mousePos, Quaternion.identity);
-                    temp.transform.SetParent(bubbleContainer);
-                    Bubble bubble = temp.GetComponent<Bubble>();
-                    bubble.init((BubbleType) selectingBubble);
 
+                        // Update currency
+                        currency -= bubbleManager.bubbleData[selectingBubble].price;
+                        currencyText.text = currency.ToString();
+                    }
 
-                    // Update currency
-                    currency -= bubbleManager.bubbleData[selectingBubble].price;
-                    currencyText.text = currency.ToString();
                 }
             }
 
-            // cancel placing bubble
-            if (Input.GetMouseButtonDown(1))
-            {
-                deselectBubble();
-            }
         }
-    }
-
-    public void resetBuilding(float currency = 100) {
-        this.currency = currency;
-        currencyText.text = currency.ToString();
-        foreach (Transform child in bubbleContainer) {
-            Destroy(child.gameObject);
-        }
+        // cancel placing bubble
+        if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.Escape))
+            deselectBubble();
     }
 
     #region btns
