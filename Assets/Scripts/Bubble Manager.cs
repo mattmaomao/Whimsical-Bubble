cc2ee5b63e0fb1dd6e7e7f3cf6e55df08d720a6e
mattmaomao <matthewcp220@gmail.com>
2025-01-25 16:27:24 +0800

    can place bubble

---
+++ Assets/Scripts/Bubble Manager.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BubbleManager : MonoBehaviour
+{
+    [SerializeField] public List<BubbleData> bubbleData;
+    [SerializeField] public List<GameObject> bubbleBtns;
+
+    void Start()
+    {
+        for (int i = 0; i < bubbleData.Count; i++)
+        {
+            bubbleBtns[i].GetComponent<Image>().sprite = bubbleData[i].sprite;
+            bubbleBtns[i].GetComponentInChildren<TMPro.TextMeshProUGUI>().text = bubbleData[i].price.ToString();
+        }
+    }
+}
+
+
+[Serializable]
+public struct BubbleData
+{
+    public string name;
+    public Sprite sprite;
+    public float price;
+    public GameObject prefab;
+}
--- Assets/Scripts/Building System.cs
+++ Assets/Scripts/Building System.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] BubbleManager bubbleManager;
+
+    [SerializeField] float currency = 1000f;
+    [SerializeField] TextMeshProUGUI currencyText;
+    [SerializeField] int selectingBubble = -1;
+    [SerializeField] GameObject cursorBubbleImg;
+    [SerializeField] Transform bubbleContainer;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (selectingBubble != -1)
+        {
+            // show bubble on cursor
+            cursorBubbleImg.SetActive(true);
+            cursorBubbleImg.transform.position = Input.mousePosition;
+
+            // place bubble
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (currency >= bubbleManager.bubbleData[selectingBubble].price)
+                {
+                    // snap posisiton to grid
+                    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    mousePos.x = Mathf.Round(mousePos.x);
+                    mousePos.y = Mathf.Round(mousePos.y);
+                    mousePos.z = 0;
+
+                    // todo: check if the position is valid
+
+                    // Instantiate bubble
+                    GameObject temp = Instantiate(bubbleManager.bubbleData[selectingBubble].prefab, mousePos, Quaternion.identity);
+                    temp.transform.SetParent(bubbleContainer);
+
+                    // Update currency
+                    currency -= bubbleManager.bubbleData[selectingBubble].price;
+                    currencyText.text = currency.ToString();
+                }
+            }
+
+            // cancel placing bubble
+            if (Input.GetMouseButtonDown(1))
+            {
+                deselectBubble();
+            }
+        }
     }
+
+    #region btns
+    public void selectBubble(int idx)
+    {
+        selectingBubble = idx;
+        cursorBubbleImg.GetComponent<Image>().sprite = bubbleManager.bubbleData[idx].sprite;
+        cursorBubbleImg.SetActive(true);
+    }
+    public void deselectBubble()
+    {
+        selectingBubble = -1;
+        cursorBubbleImg.SetActive(false);
+    }
+    #endregion
 }
